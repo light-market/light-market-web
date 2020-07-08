4562b0c30e5bd3011fab6a4b448cc34a2356e90c
unknown <mo.metwaly94@gmail.com>
2020-07-08 16:51:12 +0200

    init basic components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { VeterinaryComponent } from './components/veterinary/veterinary.component';
+import { CategoryItemComponent } from './pages/category-item/category-item.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/categories', pathMatch: 'full' },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'veterinary', component: VeterinaryComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent },
+  { path: 'categories/:category', component: CategoryItemComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/categories/categories.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.scss']
+})
+export class CategoriesComponent implements OnInit {
+  categories = ['Cats', 'Dogs', 'Birds', 'Products']
+  images = ['https://zjf683hopnivfq5d12xaooxr-wpengine.netdna-ssl.com/wp-content/uploads/2020/02/GettyImages-1199242002-1-1480x833.jpg',
+    'https://www.sciencemag.org/sites/default/files/styles/article_main_image_-_1280w__no_aspect_/public/dogs_1280p_0.jpg?itok=6jQzdNB8',
+    'https://images2.minutemediacdn.com/image/upload/c_crop,h_1188,w_2121,x_0,y_142/f_auto,q_auto,w_1100/v1554733134/shape/mentalfloss/78996-istock-682216682.jpg',
+    'http://imaginepetproducts.ca/img/imagine-products.png'];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
